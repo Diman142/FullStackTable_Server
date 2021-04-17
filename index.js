eb1dfd3d4e30308eaa080b4a1a7b9799a5509338
Diman142 <Diman142222@gmail.com>
2021-04-17 17:37:22 +0300

    created

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const app = express();
+const mysql = require('mysql');
+
+const PORT = process.env.PORT || 3001
+
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'Pr0gresstech00++',
+  database: 'users',
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.end('<h1>Hello Server</h1>')
+})
+
+app.get('/api/get', (req, res) => {
+  const sqlSelect = "SELECT * FROM users_reg"
+  db.query(sqlSelect, (err, result) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(result)
+    }
+
+  })
+})
+
+
+app.get('/api/calc', (req, res) => {
+  const sqlSelect = "SELECT COUNT(UserID) as users FROM users_reg UNION SELECT COUNT(UserID) FROM users_reg WHERE DATEDIFF(Last_Activity, Data_Registration) > 7"
+  db.query(sqlSelect, (err, result) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(result)
+    }
+  })
+})
+
+app.get("/api/delete", (req, res) => {
+  let sqlDelete = "TRUNCATE TABLE users_reg"
+
+  db.query(sqlDelete, (err, result) => {
+
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(result)
+    }
+
+  })
+})
+
+app.post("/api/insert", (req, res) => {
+
+  let sqlInsert = "INSERT INTO users_reg (UserID, Data_Registration, Last_Activity) VALUES"
+
+  req.body.newInfo.forEach((params, index) => {
+
+    let str = ""
+
+    if (index == req.body.newInfo.length - 1) {
+      str = `("${params.UserID}", "${params.Data_Registration}", "${params.Last_Activity}");`
+    } else {
+      str = `("${params.UserID}", "${params.Data_Registration}", "${params.Last_Activity}"),`
+    }
+
+    sqlInsert = `${sqlInsert} ${str}`
+  })
+
+
+  db.query(sqlInsert, (err, result) => {
+
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(result)
+    }
+
+  })
+})
+
+
+
+app.listen(PORT, () => {
+  console.log('runnig in port 3001')
+})
+
+
